cd1e6f289e23c5becaac9926c57e7da7b6ab0ddf
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-04-07 15:42:33 +0300

    add activity_questions

--- app/src/main/java/com/rorono/quiz/QuestionsActivity.kt
+++ app/src/main/java/com/rorono/quiz/QuestionsActivity.kt
@@ -2,10 +2,14 @@ package com.rorono.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class QuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questions)
+
+        val questionList = Constants.getQuestions()
+        Log.i("Qustions","${questionList.size}" )
     }
 }
