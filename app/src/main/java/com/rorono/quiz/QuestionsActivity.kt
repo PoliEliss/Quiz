2aab6f8e0eabfabe93c786630ea0728b5b975c9d
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-04-10 23:06:28 +0300

    create selected the right and wrong answers and displaying the nest question

--- app/src/main/java/com/rorono/quiz/QuestionsActivity.kt
+++ app/src/main/java/com/rorono/quiz/QuestionsActivity.kt
@@ -1,15 +1,151 @@
 package com.rorono.quiz
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.rorono.quiz.databinding.ActivityQuestionsBinding
 
-class QuestionsActivity : AppCompatActivity() {
+class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.tv_option_one -> {
+                selectedOptionView(binding.tvOptionOne, 1)
+            }
+            R.id.tv_option_two -> {
+                selectedOptionView(binding.tvOptionTwo, 2)
+            }
+            R.id.tv_option_three -> {
+                selectedOptionView(binding.tvOptionThree, 3)
+            }
+            R.id.tv_option_four -> {
+                selectedOptionView(binding.tvOptionFour, 4)
+            }
+            R.id.btn_submit -> {
+                if (mSelectedOptionPosition == 0) {
+                    mCurrentPosition++
+
+                    when {
+                        mCurrentPosition <= mQuesrionsList!!.size -> {
+                            setQuestion()
+                        }
+                        else -> {
+                            Toast.makeText(
+                                this,
+                                "You have successfully completed the Quiz ",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                } else{
+                    val question = mQuesrionsList?.get(mCurrentPosition-1)
+                    if (question!!.correctAnswer !=mSelectedOptionPosition){
+                        answerView(mSelectedOptionPosition,R.drawable.wrong_option_border_bg)
+                    }
+                    answerView(question.correctAnswer,R.drawable.correct_option_border_bg)
+                    if (mCurrentPosition == mQuesrionsList!!.size){
+                        binding.btnSubmit.text = "FINISH"
+                    }else{
+                        binding.btnSubmit.text = "GO TO NEXT QUESTION"
+                    }
+                    mSelectedOptionPosition = 0
+                }
+
+            }
+        }
+    }
+
+    private fun answerView(answer: Int, drawableView: Int) {
+        when (answer) {
+            1 -> {
+                binding.tvOptionOne.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            2 -> {
+                binding.tvOptionTwo.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            3 -> {
+                binding.tvOptionThree.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            4 -> {
+                binding.tvOptionFour.background = ContextCompat.getDrawable(this, drawableView)
+            }
+        }
+
+    }
+
+
+    private var mCurrentPosition: Int = 1
+    private var mQuesrionsList: ArrayList<Qustion>? = null
+    private var mSelectedOptionPosition: Int = 0
+
+    lateinit var binding: ActivityQuestionsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_questions)
+        binding = ActivityQuestionsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val questionList = Constants.getQuestions()
-        Log.i("Qustions","${questionList.size}" )
+        mQuesrionsList = Constants.getQuestions()
+
+        setQuestion()
+
+        binding.tvOptionOne.setOnClickListener(this)
+        binding.tvOptionTwo.setOnClickListener(this)
+        binding.tvOptionThree.setOnClickListener(this)
+        binding.tvOptionFour.setOnClickListener(this)
+
+        binding.btnSubmit.setOnClickListener(this)
     }
+
+    private fun setQuestion() {
+
+        val question = mQuesrionsList!![mCurrentPosition - 1]
+        defaultOptionsView()
+
+        if (mCurrentPosition == mQuesrionsList!!.size){
+            binding.btnSubmit.text = "FINISH"
+        }else {
+            binding.btnSubmit.text = "SUBMIT"
+
+        }
+        binding.progressBar.progress = mCurrentPosition
+        binding.tvProgress.text = "$mCurrentPosition" + "/" + binding.progressBar.max.toString()
+        binding.tvQuestion.text = question!!.question
+        binding.ivImage.setImageResource(question.image)
+        binding.tvOptionOne.text = question.optionOne
+        binding.tvOptionTwo.text = question.optionTwo
+        binding.tvOptionThree.text = question.optionTree
+        binding.tvOptionFour.text = question.optionFour
+    }
+
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView>()
+        options.add(0, binding.tvOptionOne)
+        options.add(1, binding.tvOptionTwo)
+        options.add(2, binding.tvOptionThree)
+        options.add(3, binding.tvOptionFour)
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this, R.drawable.defalt_option_border_bg
+            )
+        }
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this, R.drawable.selected_option_border_bg
+        )
+    }
+
 }
